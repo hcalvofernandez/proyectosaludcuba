e6f9653988a81c430b847318aec6eb2f86d59570
dmontero <dmontero@quanam.com>
2020-08-02 15:55:20 -0300

    Migracion parcial de modelos

---
+++ health_ophthalmology/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name' : 'GNU Health ophthalmology package',
+    'summary': '',
+    'description': """GNU Health ophthalmology package
+    """,
+    'version': '1.0.1',
+    'category': 'Hospital Management System',
+    'author': 'Almighty Consulting Solutions Pvt. Ltd.',
+    'support': 'health@gnusolidario.org',
+    'website': 'https://www.gnuhealth.org',
+    'license': 'OPL-1',
+    'depends' : [
+        'base',
+    ],
+    'data' : [
+        'views/template.xml',
+        'views/hms_view.xml',
+    ],
+    'images': [
+        'static/description/hms_document_preview_almightycs_cover.jpg',
+    ],
+    'application': False,
+    'sequence': 2,
+    'price': 36,
+    'currency': 'EUR',
+}

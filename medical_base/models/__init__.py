354f9ab2c439797d329e45b6cfbc847185cd7fd1
atg <ariel.freedom@gmail.com>
2020-08-23 20:47:30 -0400

    [REF][ADD] medical_genetics and medical_base

---
+++ medical_base/models/__init__.py
@@ -0,0 +1,7 @@
+# Copyright 2011-2020 GNU Solidario <health@gnusolidario.org>
+# Copyright Copyright 2020 NeoHan Solutions Cuba
+# License GPL-3.0 or later (http://www.gnu.org/licenses/gpl.html).
+#  para GNU Solidario  <health@gnusolidario.org>
+
+from . import medical_base_institution
+from . import medical_base_pathology
